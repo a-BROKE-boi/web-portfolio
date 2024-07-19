447c53afa3a273d01fb6f8996eb55f4a8f60108f
Caio <caiowhitmore@outlook.com>
2024-07-19 09:52:48 +0100

    boiler plate

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import styles from "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className={styles.App}>
+        <header>hello world</header>
+        <body>crazy</body>
+      </div>
+    </>
+  );
+}
+
+export default App;
