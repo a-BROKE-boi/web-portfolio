d20cbfa3f08e5aa2b4c12a3614211a8943570c6c
Caio <caiowhitmore@outlook.com>
2024-07-25 15:39:27 +0100

    added scew animations

--- react-portfolio/src/components/Body/Body.jsx
+++ react-portfolio/src/components/Body/Body.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 
 import styles from "./Body.module.css";
+import { motion } from "framer-motion";
 
 export const Body = () => {
+  const text = "Framer Motion is a really cool tool".split(" ");
   return (
     <>
-      {/* <div className={styles.menu}>
-        <div className={styles.toggle}></div>
-        <li style="--i:0">
-          <a href="#"></a>
-        </li>
-      </div> */}
+      <ul className={styles.menu}>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Milk</li>
+      </ul>
+      <div className={styles.box}>
+        <h2>Parallelogram CSS</h2>
+        <div className={styles.parallelogram}></div>
+        <div className={styles.child}>
+          <h2 className={styles.text}>hello</h2>
+        </div>
+      </div>
+      <h2>
+        {text.map((el, i) => (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.25,
+              delay: i / 10,
+            }}
+            key={i}
+          >
+            {el}{" "}
+          </motion.span>
+        ))}
+      </h2>
     </>
   );
 };
