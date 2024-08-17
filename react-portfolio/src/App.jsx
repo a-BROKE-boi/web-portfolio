b2b057e18600e71a242f4bfdc7970503950aea52
Caio <caiowhitmore@outlook.com>
2024-08-17 15:28:54 +0100

    scrapped initial body and added hero section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,13 @@
 import styles from "./App.module.css";
 import { Body } from "./components/Body/Body";
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Body />
+      <Hero />
     </div>
   );
 }
--- react-portfolio/src/components/Body/Body.jsx
+++ react-portfolio/src/components/Body/Body.jsx
@@ -5,30 +5,5 @@ import { motion } from "framer-motion";
 
 export const Body = () => {
   const text = "Framer Motion is a really cool tool".split(" ");
-  return (
-    <>
-      <div className={styles.flexContainer}>
-        <div className={styles.flexChild}>
-          {" "}
-          <div className={styles.box}>
-            <h2 className={styles.text}>Parallelogram CSS</h2>
-            <div className={styles.parallelogram}></div>
-            <div className={styles.child}>
-              <h2 className={styles.text}>hello</h2>
-            </div>
-          </div>
-        </div>
-        <div className={styles.flexChild}>
-          {" "}
-          <div className={styles.container}>
-            <img
-              className={styles.imageAlignRight}
-              src="src/assets/Gigachad-PNG-Images.png"
-              alt="Cool guy"
-            ></img>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 };
